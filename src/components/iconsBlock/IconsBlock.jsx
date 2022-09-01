ee3665fb643b02cc0072f53fc602521eb51c1d9c
Andrew <andrew.novikov@outlook.com>
2022-09-01 22:17:39 +0300

    правки стилей

--- src/components/iconsBlock/IconsBlock.jsx
+++ src/components/iconsBlock/IconsBlock.jsx
@@ -2,7 +2,7 @@ import { ReactComponent as CartSvg } from '../../assets/svg/cart.svg';
 import { ReactComponent as FavoriteSvg } from '../../assets/svg/favorite.svg';
 import { ReactComponent as CompareSvg } from '../../assets/svg/compare.svg';
 import { useSelector } from 'react-redux';
-import './iconsBlock.css';
+import styles from './IconsBlock.module.scss';
 import { Link } from 'react-router-dom';
 
 export default function IconsBlock() {
@@ -11,10 +11,10 @@ export default function IconsBlock() {
     const compare = useSelector(state => state.compare)
 
     return (
-        <div className='container'>
-            <Link to='cart'><CartSvg className='cart' /><span>{cart.length }</span>   </Link>
-            <Link to='favorite'> <FavoriteSvg className='favorite' /><span>{favorite.length}</span> </Link>
-            <Link to='compare'><CompareSvg className='compare' /><span>{compare.length}</span>  </Link>
+        <div className={styles.container }>
+            <Link to='cart'><CartSvg className={styles.svg } /><span>{cart.length }</span>   </Link>
+            <Link to='favorite'> <FavoriteSvg className={styles.svg} /><span>{favorite.length}</span> </Link>
+            <Link to='compare'><CompareSvg className={styles.svg} /><span>{compare.length}</span>  </Link>
         </div>
         );
 }
--- src/components/popProduct/PopProducts.jsx
+++ src/components/popProduct/PopProducts.jsx
@@ -1,18 +1,22 @@
 import popProduct from '../../data/popProduct.json';
 import styles from './PopProduct.module.scss';
 import { Link } from 'react-router-dom';
+import { ReactComponent as CartSvg } from '../../assets/svg/cart.svg';
 
 export default function PopProduct() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.main_title }>популярные товары</h1>
+            <h1 className={styles.main_title}>популярные товары</h1>
             {popProduct.map((product, index) =>
-                <div className={styles.wrapper} key={index }>
-                    <img className={styles.img} alt='image' src={process.env.PUBLIC_URL + `${product.img}` } />
-                    <Link to={product.link} className={styles.title }>{product.title}</Link>
-                    <p className={styles.price }>{product.price}</p>
+                <div className={styles.wrapper} key={index}>
+                    <img className={styles.img} alt='image' src={process.env.PUBLIC_URL + `${product.img}`} />
+                    <Link to={product.link} className={styles.title}>{product.title}</Link>
+                    <p className={styles.price}>{product.price}</p>
+                    <div>
+                        <button><CartSvg /></button>
+                    </div>
                 </div>
-)}
-            </div>
-        );
+            )}
+        </div>
+    );
 }
