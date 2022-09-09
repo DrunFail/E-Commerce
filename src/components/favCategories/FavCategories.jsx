aceaa68a52b1c9e6cab90a6b5f3ffe3c2b9cbcf2
Andrew <andrew.novikov@outlook.com>
2022-09-09 17:50:05 +0300

    правки стилей

--- src/components/favCategories/FavCategories.jsx
+++ src/components/favCategories/FavCategories.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
 import './grid.css';
 import { Link } from 'react-router-dom';
+import CatalogMenu from '../Catalog/CatalogMenu/CatalogMenu';
 
 export default function FavCategories() {
     const fav = useSelector(state => state.fav)
     return (
         <div id='fav'>
+            <CatalogMenu />
            <h6>избранные категории</h6>
             {fav.map((elem, index) => <Link to={elem.link } key={index }>{elem.title}</Link>)}
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import CatalogMenu from "../components/Catalog/CatalogMenu/CatalogMenu";
 import FavCategories from "../components/favCategories/FavCategories";
 import PopProduct from "../components/popProduct/popProductList/PopProductList";
 import PromoSlider from "../components/promoSlider/PromoSlider";
@@ -6,7 +5,7 @@ import PromoSlider from "../components/promoSlider/PromoSlider";
 export default function Home() {
     return (
         <>
-            <CatalogMenu />
+            
             <FavCategories />
             <PromoSlider />
             <PopProduct />
