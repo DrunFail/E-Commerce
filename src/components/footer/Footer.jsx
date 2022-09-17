5bdb305d5810e513048d4eba7a15ee7d0e3a2e14
Andrew <andrew.novikov@outlook.com>
2022-09-17 22:15:03 +0300

    адаптив футера. реализовано скрытие пунктов меню

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,14 @@
 import styles from './Footer.module.scss';
 import footerMenu from '../../data/footerMenu';
-import { Link } from 'react-router-dom';
+import FooterCardMenu from './footerCardMenu/FooterCardMenu';
 
 export default function Footer() {
+    
     return (
         <footer className={styles.footer}>
             <div>
                 {footerMenu.map((menu, index) =>
-                    <div key={index} className={styles.sell}>
-                        <p className={styles.title}>{menu.title}</p>
-                        {menu.links.map((lin, index) =>
-                            <Link key={index} className={styles.link} to={lin.link}>{lin.name}</Link>
-                        )}
-                    </div>
+                    <FooterCardMenu key={index} menu={menu }/>
                 )}
             </div>
            
