bec2431247de669b26d0cc4b984a2fabd1d2bdbc
Andrew <andrew.novikov@outlook.com>
2022-10-21 18:33:06 +0300

    refactoring navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import navMenu from '../../data/navMenu.json';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
+    const navLinks = [
+        {
+            "title": "Акции",
+            "link": "/promo"
+        },
+        {
+            "title": "Услуги",
+            "link": "/services"
+        },
+        {
+            "title": "Магазины",
+            "link": "/shops"
+        },
+        {
+            "title": "Форум",
+            "link": "/forum"
+        }
+    ]
+
+    const currentLocation = useLocation();
+    const {pathname } = currentLocation
+    
+
+
     return (
-        <nav className={styles.nav }>
-            {navMenu.map((nav, index) =>
-                <Link
-                    key={index}
-                    className={styles.link}
-                    to={nav.link}>
-                    {nav.title}
-                </Link>
-            )}
+        <nav
+            className={styles.nav}
+        >
+            <ul role='list'>
+                {navLinks.map((link, index) => {
+                    const aria = (pathname === link.link) ? "page"  : undefined
+                    return (
+                        <li
+                            key={index}
+                        >
+                            <Link
+                                className={styles.link}
+                                to={link.link}
+                                aria-current={aria}
+                            >
+                                {link.title}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </nav>
     );
 }
