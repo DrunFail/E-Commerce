0046cac85901b0f92b3a8fde346c7d880b3bbdeb
Andrew <andrew.novikov@outlook.com>
2022-10-21 18:52:43 +0300

    refactoring styles favCategories

--- src/components/favCategories/FavCategories.tsx
+++ src/components/favCategories/FavCategories.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../redux/hooks';
 import { selectFav } from '../../redux/slices/fav/favSlice';
-import './grid.css';
+import styles from './FavCategories.module.scss';
 
 
 export default function FavCategories() {
     const fav = useAppSelector(selectFav)
     return (
-        <div id='fav'>
-            
-           <h6>избранные категории</h6>
-            {fav.map((elem, index) => <Link to={elem.link } key={index }>{elem.title}</Link>)}
+        <div className={styles.fav}>
+            <h6>избранные категории</h6>
+            {fav.map((elem, index) =>
+                <Link
+                    key={index}
+                    to={elem.link}
+                >
+                    {elem.title}
+                </Link>
+            )}
         </div>
     );
 }
